b083e6cbb770666edcf02ef83859a271949c37c1
AshwiniChavhan9 <ashwini.chavhan910@gmail.com>
2021-11-16 12:27:48 +0530

    TemplateView added

--- class_based_views/basic_app/views.py
+++ class_based_views/basic_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 # Create your views here.
 
-class CBViews(View):
-    def get(self,request):
-        return HttpResponse("Class based views are cool !")
+class IndexView(TemplateView):
+    template_name='index.html'
