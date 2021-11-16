e6ab4c1879f561a13769ed3ce2da8bd3a53f40d5
AshwiniChavhan9 <ashwini.chavhan910@gmail.com>
2021-11-16 13:03:37 +0530

    models created

--- class_based_views/basic_app/views.py
+++ class_based_views/basic_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 class IndexView(TemplateView):
-    template_name='index.html'
+    template_name='basic_app/index.html'
 
     def get_context_data(self, **kwargs):
         context=super().get_context_data(**kwargs)
