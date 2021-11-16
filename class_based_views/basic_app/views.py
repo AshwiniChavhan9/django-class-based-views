d5a56f2829c69863724ae6c80d3b92052098f29c
AshwiniChavhan9 <ashwini.chavhan910@gmail.com>
2021-11-16 12:37:16 +0530

    TemplateViews with context dictionary

--- class_based_views/basic_app/views.py
+++ class_based_views/basic_app/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 class IndexView(TemplateView):
     template_name='index.html'
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['inject_me']="Basic Injection !"
+        return context
